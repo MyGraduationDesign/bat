24a1d2a346b8f53ed0b2d35b106eecd48e98be1b
liuxg3 <liuxg3@asiainfo.com>
2018-05-28 22:45:58 +0800

    添加启动程序脚本与窗口选择脚本

---
+++ start.bat
@@ -0,0 +1,11 @@
+echo start
+
+echo QQ START
+start /d "C:\Program Files (x86)\Tencent\QQ\Bin" QQ.exe
+
+echo WeChat START
+start /d "C:\Program Files (x86)\Tencent\WeChat" WeChat.exe
+
+echo timeMachine START
+
+exit
